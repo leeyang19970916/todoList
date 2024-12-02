c85a986cece663d7c56da85e3a3717ae7fb9648c
leeyang19970916 <91140699+leeyang19970916@users.noreply.github.com>
2024-12-02 18:42:05 +0800

    useContext building

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { TodoProvider} from "./store/todoContext";
 import TodoContainer from "./component/todoContainer";
+import { DiaLogProvider } from "./store/diaLogContext";
 
 export const App: React.FC = () => {
   return (
+    <DiaLogProvider>
     <div className="relative h-[100vh] flex flex-col flex-nowrap">
       <div className="p-[1rem_3rem] bg-gray-100">
         <h1 className="text-2xl font-bold">Todo List</h1>
@@ -12,5 +14,6 @@ export const App: React.FC = () => {
         <TodoContainer />
       </TodoProvider>
     </div>
+    </DiaLogProvider>
   );
 };
